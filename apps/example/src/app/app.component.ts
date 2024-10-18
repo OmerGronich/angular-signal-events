add3b4e90a6172a7b3595fd61514bf2c7787d547
omergronich <gronichomer@gmail.com>
2024-10-18 16:20:13 +0300

    initial commit

--- apps/example/src/app/app.component.ts
+++ apps/example/src/app/app.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import {Component} from '@angular/core';
+import {event, subject} from "angular-events";
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'example';
+  increment = event<number>()
+  onMessage = this.increment.on((delta) => `Increment by ${delta}`);
+  count = subject(0, this.increment.on((delta) => (count) => count + delta))
+
+  constructor() {
+    this.onMessage(message => console.log(`Message emitted:`, message))
+  }
 }
---
+++ packages/angular-events/src/index.ts
@@ -0,0 +1,99 @@
+import {Observable, Subject} from "rxjs";
+import {assertInInjectionContext, computed, DestroyRef, inject, Injector, signal, Signal} from "@angular/core";
+
+export type Handler<E> = (<O>(
+  transform: (e: E) => Promise<O> | O
+) => Handler<O>) & {
+  $: Observable<E>;
+};
+
+export type Emitter<E> = (e: E) => void;
+
+export class HaltError extends Error {
+  constructor(public reason?: string) {
+    super(
+      reason
+        ? "Event propagation halted: " + reason
+        : "Event propagation halted"
+    );
+  }
+}
+
+export function halt(reason?: string): never {
+  throw new HaltError(reason);
+}
+
+function makeHandler<E>($: Observable<E>, injector: Injector): Handler<E> {
+  function handler<O>(transform: (e: E) => Promise<O> | O): Handler<O> {
+    const next$ = new Subject<O>();
+    const sub = $.subscribe((e) => {
+      try {
+        const res = transform(e);
+        if (res instanceof Promise) res.then((o) => next$.next(o));
+        else next$.next(res);
+      } catch (e) {
+        if (!(e instanceof HaltError)) throw e;
+        console.info(e.message);
+      }
+    });
+    injector.get(DestroyRef).onDestroy(() => sub.unsubscribe());
+    return makeHandler<O>(next$, injector);
+  }
+
+  handler.$ = $;
+  return handler;
+}
+
+
+export interface AngularEvent<E> {
+  on: Handler<E>;
+  emit: Emitter<E>
+}
+
+export function event<E>(_injector?: Injector): AngularEvent<E> {
+  _injector || assertInInjectionContext(event)
+  const injector = _injector ?? inject(Injector)
+  const $ = new Subject<E>();
+  return {
+    on: makeHandler($, injector),
+    emit: (e) => $.next(e),
+  } as const
+}
+
+export function subject<T>(
+  init: T,
+  ...events: Array<Handler<T | ((prev: T) => T)>>
+): Signal<T>;
+export function subject<T>(
+  init: () => T,
+  ...events: Array<Handler<T | ((prev: T) => T)>>
+): Signal<T>;
+export function subject<T>(
+  init: undefined,
+  ...events: Array<Handler<T | ((prev: T) => T)>>
+): Signal<T | undefined>;
+export function subject<T>(
+  init: T | undefined,
+  ...events: Array<Handler<T | ((prev: T) => T)>>
+): Signal<T | undefined>;
+export function subject<T>(
+  init: (() => T) | T | undefined,
+  ...events: Array<Handler<T | ((prev: T) => T)>>
+) {
+  if (typeof init === "function") {
+    const comp = computed(() =>
+      subject((init as () => T)(), ...events)
+    );
+    return () => comp()();
+  } else {
+    const sig = signal(init)
+    events.forEach((h) => h(v => {
+      if (typeof v === "function") {
+        sig.update(v as (value: T | undefined) => T | undefined)
+      } else {
+        sig.set(v as T);
+      }
+    }));
+    return sig.asReadonly();
+  }
+}
